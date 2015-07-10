14e2c00de93bf272a869154afe3ded59bd89f4d1
Leopoldo Jimenez Caballero <leopoldo.jc.83@gmail.com>
2015-07-10 19:10:35 +0200

    Ejercicio obligatorio

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
+var quizAuthor = require('../creditos/quiz_creditos');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
@@ -10,5 +12,6 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', quizAuthor.author);
 
 module.exports = router;
