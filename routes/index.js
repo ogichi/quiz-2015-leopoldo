aeb7c67016b636342cda24929078c41e5eb6cd8d
Leopoldo Jimenez Caballero <leopoldo.jc.83@gmail.com>
2015-07-23 20:16:29 +0200

    Multiples preguntas

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,13 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quiz
+
+// Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author', quizAuthor.author);
 
 module.exports = router;
